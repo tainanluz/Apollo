5a9cafa5330f832172a96a1c975942b24a69f255
leo-nardow <leonardoalmeida2703@gmail.com>
2021-11-11 21:12:37 -0300

    [Feature Leo: Task - Initial Commit]

---
+++ src/test/java/com/Apollo/Apollo/ApolloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Apollo.Apollo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApolloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
